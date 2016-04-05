aff555da09c060bc8f5a0bacce4ff1ef2a80684d
Nick Gaswirth <nick@roundhouse-designs.com>
2016-04-05 13:56:32 -0400

    Finisehd!

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/boilerplate.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,700,300italic" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+		<div id="page">
+			<main id="primary">
+				<div id="content">
+					<h1 id="site-title">Sklar &amp; Beguelin</h1>
+					<p>Composer Matthew Sklar and Lyricist / Bookwriter Chad Beguelin have been writing musicals together for over twenty years. The Tony Award-Nominated team wrote the scores for the Broadway musicals ELF and THE WEDDING SINGER. Their upcoming projects include THE PROM and GOTTA DANCE.</p>
+					<p>Other credits include THE RHYTHM CLUB (Signature Theater Company), JUDAS & ME (NYMF) and WICKED CITY (MASON STREET WAREHOUSE).</p>
+					<p>They are the recipients of The Jonathan Larson Performing Arts Foundation Award, The Edward Kleban Award, the Gilman & Gonzalez-Fall Musical Theater Award and ASCAP Foundation Richard Rodgers New Horizons Award.</p>
+
+					<div id="links">
+						<div class="ghost-button"><a href="//matthewsklar.com">Visit Matthew Sklar&rsquo;s Website</a></div>
+						<div class="ghost-button"><a href="//dev.roundhouse-designs.com/chadbeguelin">Visit Chad Beguelin&rsquo;s Website</a></div>
+					</div>
+
+					<footer id="footer-desktop" class="colophon">
+						<div class="site-info">
+							<p>&copy;<?php echo date('Y'); ?> Matthew Sklar and Chad Beguelin<br />
+							Site by <a href="https://roundhouse-designs.com" target="_blank">Roundhouse Designs</a></p>
+						</div>
+					</footer>
+				</div>
+			</main>
+			<aside id="image">
+				<img src="img/sklarandbeguelin.jpg" alt="Matthew Sklar and Chad Beguelin" />
+			</aside>
+			<footer id="footer-mobile" class="colophon">
+				<div class="site-info">
+					<p>&copy;<?php echo date('Y'); ?> Matthew Sklar and Chad Beguelin<br />
+					Site by <a href="https://roundhouse-designs.com" target="_blank">Roundhouse Designs</a></p>
+				</div>
+			</footer>
+		</div>
+
+        <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/vendor/FitText.js/jquery.fittext.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='https://www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
+    </body>
+</html>
